e6a54f2ac9819593bbacf52eba30c4099c71cc18
Juan I. Bartamian <juan.i.bartamian@gmail.com>
2021-05-24 21:24:53 -0300

    actualizacion de marian

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Fondo/>
+    <Navbar />
+    <router-view></router-view>
+
+    <div>
+      <Footer/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Front/Navbar.vue'
+import Fondo from './components/Front/Fondo.vue'
+import Footer from './components/Front/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Fondo,
+    Footer
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
 }
 </style>
---
+++ src/components/Front/Fondo.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+
+  <section class="src-components-front-fondo">
+    <div class="banner text-center d-block">
+      <img class="banner-logo" src="@/assets/logo.png">
+    </div>
+    
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-front-fondo',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="css">
+  .src-components-front-fondo {
+
+  }
+  .banner{
+    background-image: url(https://cdn.pixabay.com/photo/2020/04/26/15/17/seamless-pattern-5095737_960_720.png);
+    background-size: contain;
+    background-repeat: repeat;
+  }
+</style>
